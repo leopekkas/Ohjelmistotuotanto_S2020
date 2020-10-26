b4f2494b8246d5f43f8b5c3a834b33eb02a38467
Leopekka Saraste <leopekkas@gmail.com>
2020-10-26 20:46:35 +0200

    Check tehtävät 14 ja 15

---
+++ viikko1/NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.ohtuesimerkki;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lbsarast
+ */
+public class StatisticsTest {
+    
+    @Before
+    public void setUp() {
+    }
+    
+    // @Test
+    // public void hello() {}
+}
