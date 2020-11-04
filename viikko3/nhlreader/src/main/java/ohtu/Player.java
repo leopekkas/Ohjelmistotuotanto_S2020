a49b3b115ecce7340734a0853dae7bd436caee26
Leopekka Saraste <leopekkas@gmail.com>
2020-11-04 17:38:12 +0200

    vko3 t.1 Done

--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,13 +3,49 @@ package ohtu;
 
 public class Player {
     private String name;
-
+    private String team;
+    private String nationality;
+    private int goals;
+    private int assists;
+    
     public void setName(String name) {
         this.name = name;
     }
 
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getTeam() {
+        return team;
     }
 
     @Override
