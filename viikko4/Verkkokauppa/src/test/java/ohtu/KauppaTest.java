f9afcb809fb6b433c51f6d2849419172002b626e
Leopekka Saraste <leopekkas@gmail.com>
2020-11-16 13:46:03 +0200

    viikko 4, tehtävä 3 done

---
+++ viikko4/Verkkokauppa/src/test/java/ohtu/KauppaTest.java
@@ -0,0 +1,79 @@
+package ohtu.verkkokauppa;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class KauppaTest {
+    
+    Pankki pankki;
+    Viitegeneraattori viite;
+    Varasto varasto;
+    
+    @Before
+    public void setUp() {
+        // luodaan ensin mock-oliot
+        pankki = mock(Pankki.class);
+
+        viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        varasto = mock(Varasto.class);
+        
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(5));   
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+    
+    @Test
+    public void kahdenOstoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kahvi", 8));
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);     // Ostetaan tuotetta numero 2 eli kahvia
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(13));
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+        
+        // sitten testattava kauppa 
+        Kauppa k2 = new Kauppa(varasto, pankki, viite);              
+
+        // tehdään ostokset
+        k2.aloitaAsiointi();
+        k2.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli maitoa
+        k2.lisaaKoriin(2);     // Ostetaan tuotetta numero 2 eli kahvia
+        k2.tilimaksu("pekka2.0", "12346");
+        verify(pankki).tilisiirto(eq("pekka2.0"), eq(42), eq("12346"), anyString(), eq(16));
+    }
+    
+}
+
+
